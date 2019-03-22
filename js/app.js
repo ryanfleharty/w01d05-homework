649784425aa58dcf7f21cc7e32fe6725a2a9f061
WillyD <sopwill3@gmail.com>
2019-03-22 15:05:31 -0600

    first commit, q/a

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// JavaScript weekend REPS!
+
+// I. VARIABLES & DATA TYPES
+// A. Q + A
+// 1
+var x = y;
+// 2
+x = z;
+// 3
+var y = x;
+// 4
+// declare is is to create a variable using "var", "let", or "const".
+// assign is to give a variable a certain value
+// define is to give a variable a new value
+// 5
+// pseudocoding is writing out each step of a code, what it will
+// be doing, in English. It is good to plan and keep things organized.
+// 6
+// whatever percent of time is necessary, so usuaslly about 60%-40%.
+
+// B. Strings
