0c350f8314ff110505194d11d16123471c0fd0d2
WillyD <sopwill3@gmail.com>
2019-03-23 10:43:12 -0600

    Booleans and the farm completed

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@
 // I. VARIABLES & DATA TYPES
 // A. Q + A
 // 1
-var x = y;
+var x = "y";
 // 2
-x = z;
+x = "z";
 // 3
 var y = x;
 // 4
@@ -27,6 +27,35 @@ secondVariable = 8;
 // the value of firstVariable is 5.
 // 2
 const yourName = "Will";
-const newStr = `Hello, my name is ${youtName}.`
+const newStr = `Hello, my name is ${yourName}.`
 
 // C. Booleans
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' === 'Name');
+console.log(true !== false);
+console.log(false === false && false === false && false === false && true);
+console.log(false === false)
+console.log(e === 'Kevin');
+console.log(a < b < c);
+console.log((a === a) < d);
+console.log(48 == '48');
+
+// D. THE FARM
+// 1-4
+const animals = "cow";
+const areYouACow = (animal) => {
+  if (animal === "cow"){
+    console.log("moooooo");
+  } else {
+    console.log("hey! you're not a cow!");
+  }
+  return animal
+}
