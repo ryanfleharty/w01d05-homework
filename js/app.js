07c60940b83fe702259d077ea7161db8c063d1a7
Destiny <dshillis1@gmail.com>
2019-03-23 17:59:12 -0600

    Q and A done

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// A. Q + A
+// How do we assign a value to a variable?
+//const variableName = variableValue
+
+// How do we change the value of a variable?
+//variable needs to start as let variableName = versus const
+//then we write let variableName = newValue
+
+// How do we assign an existing variable to a new variable?
+// let existingVariable = newVariable
+
+// Remind me, what are declare, assign, and define?
+//declare is the first time you tell the program to create a 
+//variable or function
+//assign is when you give a variable a new value
+//define is the first time you give a variable a value
+
+// What is pseudocoding and why should you do it?
+//Pseudocoding is writing out in plain English the steps you 
+//want to take to achieve a goal. It is useful because it allows
+//you to think through a problem before jumping into code. That
+//way you can double check that each of your steps is being 
+//accomplished when you test your code.
+
+// What percentage of time should be spent thinking about how 
+//you're going to solve a problem vs actually typing in code 
+//to solve it?
+//75% - 90% of your time should be planning and 10%-25% should
+//be typing code.
+
