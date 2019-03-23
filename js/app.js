1c559c54c65ac70363b1537e305e0001f31afc0a
Adam Wolfman <adamwolfman@Adams-MBP.domain>
2019-03-22 18:39:43 -0600

    Strings Answered

--- js/app.js
+++ js/app.js
@@ -2,20 +2,61 @@ console.log("Homework 5");
 
 // I. Variables & Datatypes
 
+// A. Q&A
+
 //1. How do we assign a value to a variable?
+//
 //   let variable = value; if variable is declared, reassign
 //   by just using the variable name, ex: variable = newValue
 
 //2. How do you reassign a value to a variable
+//
 //   use the variable name and equals
 //   variable = newValue
 
 //3. How do you assign an existing variable to a new variable? 
+//
 //   let newVariable = existingVar
 
 //4. What are declare, assign, and define
+//
 //   declare: declare is when you use let/const/var to first give a variable a value
 //   assign: assign is when you take an existing variable and give it a new value
 //   define: the definition shows where the variable gets stored
 
-//5. 
+//5. What is psuedocoding and why is it important? 
+//   Pseudocoding is the process of writing down the steps to 
+//   solve a coding problem in English. It is important so that 
+//   your thinking doesn't get bogged down by syntax and you are
+//   able to more easily think through the steps you need to take
+//   in order to solve it. 
+
+//6. What percentage of time should be spent thinking about how 
+//   you're going to solve a problem vs actually writing the code? 
+//   
+//   I would say about 50%/50%. The most important part should be 
+//   the thinking behind what you are trying to code. If you are
+//   just jumping in to writing the code you are likely going to 
+//   miss important steps. However, in this beginning stage of 
+//   learning, the writing of the code takes longer, so maybe that
+//   adjusts the percentage of time coding to a greater percentage. 
+
+
+//B. Strings
+
+//1. Create a variable called firstVariable. 
+
+let firstVariable = 'Hello World';
+firstVariable = 23;
+secondVariable = firstVariable;
+secondVariable = 'the value of firstVariable is still 23';
+
+//2. Create a variable called yourName and set it equal to your name
+//   as a string. Then write an exp that takes the string and returns 
+//   concatenated. 
+
+let yourName = 'Adam';
+console.log("Hello, my name is " + yourName);
+
+
+
