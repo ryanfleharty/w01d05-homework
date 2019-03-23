c032ed25ec5e83054997f960191182613e8e038f
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-03-23 08:34:58 -0600

    1 C. Booleans

--- js/app.js
+++ js/app.js
@@ -34,14 +34,35 @@ console.log("js works");
 //   - Store the value of`firstVariable` in a new variable called`secondVariable`
 //   - Change the value of`secondVariable` to any string. 
 //   - What is the value of`firstVariable` ?
-let firstVariable = "Hello World";
-    firstVariable = 4;
-let secondVariable = firstVariable;
-    secondVariable = "four";
+// let firstVariable = "Hello World";
+//     firstVariable = 4;
+// let secondVariable = firstVariable;
+//     secondVariable = "four";
     // firstVariable; => 4
 
 // 2. Create a variable called`yourName` and set it equal to your name as a string.Then, write an expression that takes the string "Hello, my name is " and the variable`yourName` so that it returns a new string with them concatenated. 
-const yourName = "Dave";
-console.log("Hello, my name is " + yourName )
+//const yourName = "Dave";
+//console.log("Hello, my name is " + yourName )
 
 // > ex: `Hello, my name is Jean Valjean`
+
+//### C.Booleans
+  //  - Using the provided variable definitions, replace the blanks so that all log statements print`true` in the console.
+    //Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print `true` to the console.
+
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a < b);
+  console.log(c > d);
+  console.log('Name' === 'Name');
+  console.log(true != false);
+  console.log(false + false + false + false + false + false != true);
+  console.log(false == false)
+  console.log(e === 'Kevin');
+  console.log(a < b < c);
+  console.log(a == a != d);
+  console.log(48  =='48');
