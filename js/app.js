9928cd7050431e1a6e2ef5c65a012ac2b1f33684
Adam Wolfman <adamwolfman@Adams-MBP.domain>
2019-03-22 18:32:31 -0600

    Q+A Answered

---
+++ js/app.js
@@ -0,0 +1,21 @@
+console.log("Homework 5");
+
+// I. Variables & Datatypes
+
+//1. How do we assign a value to a variable?
+//   let variable = value; if variable is declared, reassign
+//   by just using the variable name, ex: variable = newValue
+
+//2. How do you reassign a value to a variable
+//   use the variable name and equals
+//   variable = newValue
+
+//3. How do you assign an existing variable to a new variable? 
+//   let newVariable = existingVar
+
+//4. What are declare, assign, and define
+//   declare: declare is when you use let/const/var to first give a variable a value
+//   assign: assign is when you take an existing variable and give it a new value
+//   define: the definition shows where the variable gets stored
+
+//5. 
