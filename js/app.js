18764dfb093255bf73213b5554bbbc809cb5bdd1
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-03-23 05:07:53 -0600

    commit #1 Q + A

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// 1. How do we assign a value to a variable ?
+    // a. Make up a name for the variable (consider the name carefully because a good name communicates its utility)
+    // b. choose how you want to declare your variable (let , const, var) 
+        //default to const, use let if you know you will need to reassign it.
+        //you should only use var if the rest of the code you are working on uses it. 
+    // c. declare the variable by typing its name and setting it equal to its value
+    // d. const variable = x
+
+// 2. How do we change the value of a variable ?
+    // reasign it using the equals sign.  example: let variable = x ; variable = y
+
+// 3. How do we assign an existing variable to a new variable ?
+    // declare a new variable and define it with the name of the existing variable 
+    // let newVariable = existingVariable
+
+// 4. Remind me, what are`declare`, `assign`, and`define` ?
+    // you declare a variable when you create it. You define the variable when you assign it a value.
+
+// 5. What is pseudocoding and why should you do it ?
+    // pseudocode is code written in english. It is helpful to use while you consider WHAT you want your code to do
+    // so you don't get destracted by syntax or how exactly the code should execute.  pseudocoding lets you more easily step through a problem and consider each step the computer will need to follow
+
+// 6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+    // Is this a trick question? What are my assumptions? Do I have experience solving similar problems? Do I need to do research? how fast can I type??
+    // a beginner should spend most of his time planning and whiteboarding and only typing it in after it is solved on paper.  With some more experiencee,
+    // it is helpful to type as you go so you can use console.log and debugger software. even then, more time should be spent planning before you start typing.
