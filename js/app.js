72cbc1d51527d0c8d0bab6e73307a10ec7a1abac
John Oro <johnmichaeloro@gmail.com>
2019-03-24 13:49:09 -0600

    Part A

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//WEEKEND HOMEWORK: WEEK 1
+//1. First, we declare the variable with const or let followed by the variable name. Then we use the assignment operator to connect it to its value. Then we define the value.
+//2. You can change the value of a variable by editing its original declaration or creating a new declaration with a new value.
+//3. You place the existing variable inside the definition of the new variable.
+//4. Declare is the first part, which contains the declarer, const or let, and the variable name. Assign is the work of the assignment operator. Define occurs after the assignment operator, declaring the value of the variable.
+//5. It's the best, that's what it is. It helps you put your thoughts in writing so you know how to code before you start coding.
+//6. 75% of coding should be spent in psuedocoding.
+
+let firstVariable = "Hello World";
+firstVariable = 36;
+let secondVariable = firstVariable;
+secondVariable = "This is a cat.";
+console.log(firstVariable);
+
+
+let yourName = "John Michael Oro";
+let greeting = "Hello, my name is " + yourName + ".";
+console.log(greeting);
