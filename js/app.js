36f386efdba3e0e5004d906452da4d2ca81dbd1f
shadiboi <saoutabachi@gmail.com>
2019-03-24 17:15:55 -0600

    I. Variables & Datatypes: Q+A

---
+++ js/app.js
@@ -0,0 +1,74 @@
+console.log("RUNNING");
+// I. Variables & Datatypes
+
+// A. Q + A
+
+// How do we assign a value to a variable?
+    let variable = "value"; //CANNONT USE CONST....ONLY VAR OR LET
+
+// How do we change the value of a variable?
+    variable = "newValue";
+// How do we assign an existing variable to a new variable?
+    let newVariable = variable;
+// Remind me, what are declare, assign, and define?
+    var something; //declare
+    var something = 12; //define
+    something = variable; //assign 
+
+
+// What is pseudocoding and why should you do it?
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+// 🔴 Commit.
+
+// B. Strings
+
+// Create a variable called firstVariable.
+// Assign it the value of the string "Hello World"
+// Change the value of this variable to some number.
+// Store the value of firstVariable in a new variable called secondVariable
+// Change the value of secondVariable to any string.
+// What is the value of firstVariable?
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+// ex: Hello, my name is Jean Valjean
+// 🔴 Commit.
+
+// C. Booleans
+
+// Using the provided variable definitions, replace the blanks so that all log statements print true in the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console.
+//   const a = 4;
+//   const b = 53;
+//   const c = 57;
+//   const d = 16;
+//   const e = 'Kevin';
+
+//   console.log(a __ b);
+//   console.log(c __ d);
+//   console.log('Name' __ 'Name');
+//   console.log(true __ false);
+//   console.log(false __ false __ false __ false __ false __ false __ true);
+//   console.log(false __ false)
+//   console.log(e ___ 'Kevin');
+//   console.log(a __ b ___ c);
+//   console.log(a __ a ___ d);
+//   console.log(48 __ '48');
+// 🔴 Commit.
+
+// D. The farm
+
+// Declare a variable animal. Set it to be either "cow" or something else.
+// Write code that will print out "mooooo" if the it is equal to cow.
+// Change your code so that if the variable animal is anything other than a cow, it will print "Hey! You're not a cow."
+// Commit.
+// 🔴 Commit.
+
+// E. Driver's Ed
+
+// Make a variable that will hold a person's age. Be semantic.
+
+// Write code that will print out "Here are the keys", if the age is 16 years or older.
+
+// If the age is younger than 16, a message should print "Sorry, you're too young."
+
+// 🔴 Commit.
+
+
