f58b794eb2c99ebba597723749d456d5a576d4d3
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-24 08:10:04 -0600

    Commit 7 - Get Even

--- js/app.js
+++ js/app.js
@@ -84,7 +84,13 @@ for(let i = 12; i < 4000; i+=3){
 	console.log(i);
 }
 
-
+//B. Get Even
+//1.
+for(let i = 1; i <= 100; i++){
+	if(i % 2 === 0){
+		console.log(i + " is an even number");
+	}
+}
 
 
 
