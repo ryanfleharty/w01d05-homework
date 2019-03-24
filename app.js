7b905d6e736a6635de3311cbebcb9a3bc3eef288
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-03-23 19:05:27 -0600

    strings

---
+++ app.js
@@ -0,0 +1,30 @@
+//How do we assign a value to a variable?
+  // the variable would come first, then the = "value" would follow
+
+// How do we change the value of a variable?
+  // you can change the value by writing the variable first, then = "new value" would follow
+
+// How do we assign an existing variable to a new variable?
+  // with variable that are declared by "const" can not be redefined a new variable
+
+// Remind me, what are declare, assign, and define?
+  //declare is to create a variable, assign and define is to set a value for the variable.
+
+// What is pseudocoding and why should you do it?
+  //you should comment out a description of the opperation so that writing code will be more efficient. (an instruction for the code written in plain english)
+
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+  //you should spend majority of the time pseudocoding, because it'll cut the amount of time figuring out what goees where and limit the amount of mistakes.
+
+  // 1. Create a variable called `firstVariable`.
+  //   - Assign it the value of the string `"Hello World"`
+  //   - Change the value of this variable to some number.
+  //   - Store the value of `firstVariable` in a new variable called `secondVariable`
+  //   - Change the value of `secondVariable` to any string.
+  //   - What is the value of `firstVariable`?
+  let firstVariable;
+  firstVariable = "hello world"
+  firstVariable = "zero"
+  zero = "secondVariable"
+  secondVariable = "bye"
+  console.log(secondVariable);
