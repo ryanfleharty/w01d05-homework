fc946fd21df09ba34562b8e43132d5d5a3643d87
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-03-23 23:56:04 -0600

    3 D changing values

--- js/app.js
+++ js/app.js
@@ -225,12 +225,27 @@ const quotes = ["quote 1", "quote 2", "quote 3"]
 
 // ### C.Accessing elements
 
-// Given the following array `const randomThings = [1, 10, "Hello", true]`
-const randomThings = [1, 10, "Hello", true]
-// 1. How do you access the 1 st element in the array ?
-randomThings[0]
-//     2. Change the value of `"Hello"`
-// to `"World"`.
-randomThings[2] = "Hello World"
-// 3. Check the value of the array to make sure it updated the array.How ? Why, yes!`console.log()`;
-console.log(randomThings)
+// // Given the following array `const randomThings = [1, 10, "Hello", true]`
+// const randomThings = [1, 10, "Hello", true]
+// // 1. How do you access the 1 st element in the array ?
+// randomThings[0]
+// //     2. Change the value of `"Hello"`
+// // to `"World"`.
+// randomThings[2] = "Hello World"
+// // 3. Check the value of the array to make sure it updated the array.How ? Why, yes!`console.log()`;
+// console.log(randomThings)
+// ###
+
+// D.Change values
+
+// Given the following array 
+const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+
+// 1. What would you write to access the 3 rd element of the array ?
+ourClass[2];
+//     2. Change the value of "Github"
+// to "Octocat"
+ourClass[4] = "Octocat"
+// 3. Add a new element, "Cloud City"
+// to the array.
+ourClass.push('Cloud City');
