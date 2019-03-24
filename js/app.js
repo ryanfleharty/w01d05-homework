4a422ff397c304b62505771f23da6de207cac6b4
Destiny <dshillis1@gmail.com>
2019-03-23 19:03:19 -0600

    II. Loops, B. Get even done

--- js/app.js
+++ js/app.js
@@ -136,23 +136,40 @@ if (personsAge < 16) {
 // A. The basics
 // Write a loop that will print out all the numbers from 0 to 
 //10, inclusive.
-for (let i = 0; i < 11; i++) {
+// for (let i = 0; i < 11; i++) {
+//     console.log(i);
+// }
+
+// // Write a loop that will print out all the numbers from 10 up 
+// //to and including 400.
+// for (let i = 10; i < 401; i++) {
+//     console.log(i);
+// }
+
+// // Write a loop that will print out every third number starting 
+// //with 12 and going no higher than 4000.
+// for (let i = 12; i < 4000; i++) {
+//     if ([i] % 3 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// B. Get even
+// Print out the even numbers that are within the range of 
+//1 - 100.
+for (let i = 1; i < 101; i++) {
+    if ([i] % 2 === 0)
     console.log(i);
 }
 
-// Write a loop that will print out all the numbers from 10 up 
-//to and including 400.
-for (let i = 10; i < 401; i++) {
-    console.log(i);
+// Adjust your code to add a message next to even numbers only 
+//that says: "<-- is an even number".
+for (let i = 1; i < 101; i++) {
+    if ([i] % 2 === 0)
+    console.log(i + "<-- is an even number");
 }
 
-// Write a loop that will print out every third number starting 
-//with 12 and going no higher than 4000.
-for (let i = 12; i < 4000; i++) {
-    if ([i] % 3 === 0) {
-        console.log(i);
-    }
-}
 
 
 
