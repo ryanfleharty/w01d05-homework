f509672c5020ebc92958acb4a23f1ed6d72ce86b
keenmaster486 <cebrock106@gmail.com>
2019-03-24 18:36:11 +0000

    Part A: QandA (initial)

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//=======w01d05-homework=======
+//=======Collin Brockway=======
+
+//A. Q&A
+//1. Using let or const, for example: "let variable = 42;"
+//2. Using the "=" operator, for example: "variable = 1776;"
+//3. Also use "=" operator, e.g. "newvar = variable;"
+//4. Wait, what?
+//5. Pseudocode is highly human-readable text that has the structure
+//   of code, and thus can serve as a guide to structuring the actual
+//   code.
+//6. Something like 3/4 thinking, 1/4 typing.
+
