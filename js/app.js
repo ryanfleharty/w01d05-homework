316fd79ca9f4ae7bf2e0e2309b18da1f3b28b5d2
shadiboi <saoutabachi@gmail.com>
2019-03-24 17:23:06 -0600

    I. Variables & Datatypes: Strings

--- js/app.js
+++ js/app.js
@@ -13,22 +13,38 @@ console.log("RUNNING");
 // Remind me, what are declare, assign, and define?
     var something; //declare
     var something = 12; //define
-    something = variable; //assign 
-
+    something = variable; //assign NOW = newValue via variable
 
 // What is pseudocoding and why should you do it?
+    
+    /*Pseudocoding is a detailed, yet readable description of what a computer program 
+    or algorithm os trying to solve. No syntax is used and it is meant for descriping the 
+    flow of the program while not getting into detail. It is necesary when planning out a complex problem`*/
+
 // What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
-// 🔴 Commit.
+    /*According to prior lab on objects, "Timewise, programming is 
+        75-90% planning, and 10-25% actually typing code.".*/
+
+// 🔴 Commit.//DONE
 
 // B. Strings
 
 // Create a variable called firstVariable.
+    let firstVariable;
 // Assign it the value of the string "Hello World"
+    firstVariable = "Hello World";
 // Change the value of this variable to some number.
+    firstVariable = 37;
 // Store the value of firstVariable in a new variable called secondVariable
+    let secondVariable = firstVariable;
 // Change the value of secondVariable to any string.
+    secondVariable = "String";
 // What is the value of firstVariable?
-// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+    //  37
+// Create a variable called yourName and set it equal to your name as a string. 
+    let yourName = "Shadi";
+// Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+    let expression = "Hello, my name is " + yourName + "!";
 // ex: Hello, my name is Jean Valjean
 // 🔴 Commit.
 
