6caacf9d673b4d6c416608a70c9934d99176e0e6
keenmaster486 <cebrock106@gmail.com>
2019-03-24 19:20:41 +0000

    Commit 3: C. Booleans

--- js/app.js
+++ js/app.js
@@ -32,3 +32,22 @@ console.log(secondVariable);
 
 const yourName = "Collin Brockway";
 console.log("Hello, my name is " + yourName);
+
+//C. Booleans
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a != b);
+console.log(c > d);
+console.log('Name' === 'Name');
+console.log(true != false);
+console.log(false || false || false || false || false || false || true);
+console.log(false === false)
+console.log(e === 'Kevin');
+console.log(a != b > c);
+console.log(a == a != d);
+console.log(48 == '48');
