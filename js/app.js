e333d4101e304b0faf4ded86656a07343a5711bb
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-03-24 11:10:01 -0600

    Q+A done

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// console.log('running');
+// A.Q + A
+// How do we assign a value to a variable ?
+
+// 1. we initially have to declare the variable with either const or let. along with giving it a name.
+// after that we give it a value to declare it such as let newVariable = 12.
+
+
+
+//     How do we change the value of a variable ?
+        // if the variable is defined as let initially we can call the variable and add its new value.
+//         How do we assign an existing variable to a new variable ?
+            // by establishing that variable and when you put the value in it would = the variable we want with the new variable we're defining.
+//             Remind me, what are declare, assign, and define ?
+                // declaring is when you say you want a new variable with let or const. 
+                // assigning is giving the variable it's name say newVariable
+                // defining is when you assign the value to the variable. let newVariable = 12
+//             What is pseudocoding and why should you do it ?
+    // psuedocode is writing down what you want the code to do to get the result. In English so you and your team can understand what the goal
+    // is with the code and have a blueprint of how to get there.
+//                 What percentage of time should be spent thinking about how you 're going to solve a problem vs actually typing in code to solve it?
+                // it should be 20% of thinking of how to code it and where to start, 40% of coding. And then another 40% of debugging. 
+
+
+                
