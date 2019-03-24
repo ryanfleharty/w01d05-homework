9969b432bf7995af18146b4b55b31f582e0d5370
Destiny <dshillis1@gmail.com>
2019-03-23 18:12:57 -0600

    b. Strings done

--- js/app.js
+++ js/app.js
@@ -28,3 +28,35 @@
 //75% - 90% of your time should be planning and 10%-25% should
 //be typing code.
 
+
+
+//B. Strings
+// Create a variable called firstVariable.
+let firstVariable
+
+// Assign it the value of the string "Hello World"
+let firstVariable = "Hello World";
+
+// Change the value of this variable to some number.
+let firstVariable = 3;
+
+// Store the value of firstVariable in a new variable called 
+//secondVariable
+let secondVariable = firstVariable;
+
+// Change the value of secondVariable to any string.
+let secondVariable = 'Here is something';
+
+// What is the value of firstVariable?
+'Here is something'
+
+// Create a variable called yourName and set it equal to your 
+//name as a string. Then, write an expression that takes the 
+//string "Hello, my name is " and the variable yourName so that 
+//it returns a new string with them concatenated.
+// ex: Hello, my name is Jean Valjean
+
+let yourName = 'Destiny Hillis';
+'Hello my name is ' + yourName + '.';
+
+
