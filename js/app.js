5790ae6ea16b39924631e3691702ca4e630d80ef
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-24 07:32:07 -0600

    Commit 1 - Q&A

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log("Day 5 HW");
+
+//Variables & Datatypes
+//A. Q & A
+//1. let/const name = value;
+//2. name = newValue;
+//3. let/const newVariable = name;
+//4. declare is the first time and uses "=", assign 
+//is after you've declared a variable and want to reassign
+//a new value
+//5. psuedocode is thinking through/writing down what your 
+//code is going to do in english in order to process/map out
+//your code before beginning to write actual code
+//6. you should spent like 75-90% of your time thinking
+//and 10-25% coding
