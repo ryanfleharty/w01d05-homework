144fa3ec6fe80e4f2cacc108503b88fee3d4cc3d
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-24 07:40:06 -0600

    Commit 2 - Strings

--- js/app.js
+++ js/app.js
@@ -13,3 +13,17 @@ console.log("Day 5 HW");
 //your code before beginning to write actual code
 //6. you should spent like 75-90% of your time thinking
 //and 10-25% coding
+
+
+//B. Strings
+//1.
+let firstVariable = "Hello World";
+firstVariable = 27;
+let secondVariable = firstVariable;
+secondVariable = "Bye";
+console.log(firstVariable);
+//the value of the first variable is still 27
+
+//2. 
+let yourName = "Alex";
+console.log("Hello, my name is " + yourName);
