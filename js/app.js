183aabbe269dfbe32334a442f18ba70d34975cfa
shadiboi <saoutabachi@gmail.com>
2019-03-24 20:33:00 -0600

    III. Arrays & Control flow: Mix it up

--- js/app.js
+++ js/app.js
@@ -298,6 +298,7 @@ ourClass[2];
 
 // 2. Change the value of "Github" to "Octocat"
 ourClass[4] = "Octocat";
+
 // 3. Add a new element, "Cloud City" to the array.
 ourClass.push("Cloud City");
 
@@ -307,19 +308,23 @@ ourClass.push("Cloud City");
 // ### E. Mix It Up
 
 // Given the following array: 
-
 const myArray = [5, 10, 500, 20];
 
 // 1. Add the string `"Egon"` to the end of the array. Add another string of your choice to the end of the array.
 myArray.push("Egon");
 myArray.push("Shadi");
+
 // 2. Remove the `5` from the beginning of the array.
+myArray.shift();
 
 // 3. Add the string `"Bob Marley"` to the beginning of the array.
+myArray.unshift("Bob Marley");
 
 // 4. Remove the string of your choise from the end of the array.
+myArray.pop();
 
-// 5. Reverse this array using `Array.prototype.reverse()`.  Did you mutate the array? What does _mutate_ mean? Did the `.reverse()` method return anything?
+// 5. Reverse this array using `Array.prototype.reverse()`. Did you mutate the array? What does _mutate_ mean? Did the `.reverse()` method return anything?
+myArray.reverse();
 
 // :red_circle: Commit.
 
