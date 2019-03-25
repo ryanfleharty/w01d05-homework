79256e9c2a47ad92a37762e8572aeb40603dd1d7
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-03-24 22:31:51 -0600

    whats in your closet?

--- app.js
+++ app.js
@@ -178,3 +178,54 @@ if(digitSz < 5){
 } else {
   console.log("Monkey");
 }
+
+// What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+// Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
+// Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
+// Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirts array.
+// In the same way, access one item from Thom's pants array.
+// Access one item from Thom's accessories array.
+// Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+// Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`);
+kristynsCloset[6] = "raybans"
+kristynsCloset.push("marshmallow peeps")
+kristynsCloset[5] = ("stained knit hat")
+console.log(kristynsCloset);
+
+console.log(thomsCloset[0][1]);
+console.log(thomsCloset[1][2]);
+console.log(thomsCloset[2][1]);
+console.log(`Thom is looking fierce in a ${thomsCloset[0][1]}, ${thomsCloset[1][2]}, and ${thomsCloset[2][1]}!`);
+thomsCloset[1][2] = "Footie Pajamas"
+console.log(thomsCloset[1]);
