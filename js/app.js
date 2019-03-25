502ea2b2d58a34d35bfc25c5db5cffd5fdad2dcf
Jake Shelton <jakeshelton@Archies-Howl.local>
2019-03-24 21:17:18 -0600

    Objects D Shopaholic

--- js/app.js
+++ js/app.js
@@ -466,3 +466,13 @@ console.table(user);
 // 1. Without changing the original user object, add a new key location to the object, and give it a value or some-or-other location (a string).
 user.location = 'Denver';
 console.table(user);
+
+// D. Shopaholic!
+// 1. Our user has purchased an item! They have purchased some "carbohydrates". Using .push(), add the string "carbohydrates" to the purchased array.
+user.purchased.push('carbohydrates');
+// 2. Our user has purchased an item! They have purchased some "peace of mind". Using .push(), add the string "peace of mind" to the purchased array.
+user.purchased.push('peace of mind');
+// 3. Our user has purchased an item! They have purchased some "Merino jodhpurs". Using .push(), add the string "Merino jodhpurs" to the purchased array.
+user.purchased.push('Merino jodhpurs');
+// 4. Console.log just the "Merino jodhpurs" from the purchased array.
+console.log(user.purchased[2]);
