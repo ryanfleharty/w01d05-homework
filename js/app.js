0bf238c1871c73d2b6aebd82eed29c390de57ae7
shadiboi <saoutabachi@gmail.com>
2019-03-24 18:14:42 -0600

    II. Loops: The Basics

--- js/app.js
+++ js/app.js
@@ -114,8 +114,93 @@ makeNoise = () => {
 // If the age is younger than 16, a message should print "Sorry, you're too young."
 
 personAge = 12;  //NOW PRINTS "SORRY..."
-    console.log(printMsg());
+console.log(printMsg());
 
-// 🔴 Commit.
+// 🔴 Commit. //DONE
+
+
+// ## II. Loops
+// Remember: **USE `let` when you initialize your for loops!**
+// This is GOOD: `for(`**`let i = 0;`**` i < 100; i++)`
+// This is NO GOOD: `for(i = 0; i < 100; i++)`
+
+// ### A. The basics
+
+// 1. Write a loop that will print out all the numbers from 0 to 10, inclusive.
+    
+    for (let i = 0; i <= 10; i ++) {
+        console.log(i);
+    };
+
+// 2. Write a loop that will print out all the numbers from 10 up to and including 400.
+    for (let i = 10; i <= 400; i ++) {
+        console.log(i);
+    };
+
+// 3. Write a loop that will print out every third number starting with 12 and going no higher than 4000.
+    for (let i = 12; i < 4000; i+= 3) {
+        console.log(i);
+    };
+
+// :red_circle: Commit.
+
+// ### B. Get even
+
+// 1. Print out the even numbers that are within the range of 1 - 100.
+
+// 2. Adjust your code to add a message next to even numbers only that says: "<-- is an even number".
+
+// :red_circle: Commit.
+
+
+// ### C. Give me Five
+
+// 1. For the numbers 0 - 100, print out "I found a `number`. High five!" if the number is a multiple of five.
+
+// >Example Output:
+// ```
+// I found a 5. High five!
+// I found a 10. High five!
+// ```
+
+// 2. Add to the code from above to print out "I found a `number`. Three is a crowd" if the number is a multiple of three.
+
+// >Example Output:
+// ```
+// I found a 3. Three is a crowd
+// I found a 5. High five!
+// I found a 6. Three is a crowd
+// I found a 9. Three is a crowd
+// I found a 10. High five!
+// ```
+
+// :red_circle: Commit.
+
+
+// ### D. Savings account
+
+// 1. Write code that will save the sum of all the numbers between 1 - 10 to a variable called `bank_account`.
+
+// >Check your work! Your banck_account should have $55 in it.
+
+// 2. You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+
+// >Check your work! Your banck_account should have $10,100 in it.
+
+// :red_circle: Commit.
+
+
+// ### E. Multiples of 3 and 5
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.  If a previous question you've done has helpful bits of code in it that partially solves this problem, look back at them.
+
+// You just solved [Project Euler](https://projecteuler.net/problem=1) problem 1!  
+
+// Are you having dejà vu? This just in! From the "Read the entire problem before you start" dept: This problem was on a previous assignment.  You may skip it **if you've already done it**, just include a comment saying that you've already done it. If you've now done the problem twice, perhaps next time you'll read the whole problem before starting it.
+
+// :red_circle: Commit.
+
 
 
