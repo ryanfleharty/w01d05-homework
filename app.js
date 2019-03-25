bfbbdafa62105dd1038bec00acf24e6cc3a606bc
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-03-24 20:25:41 -0600

    talk about it

--- app.js
+++ app.js
@@ -106,3 +106,16 @@ let bankAccount; //declare a variable
     sum += i*2; //sum amount
   }
   console.log(sum);
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000. If a previous question you've done has helpful bits of code in it that partially solves this problem, look back at them.
+// You just solved Project Euler problem 1!
+// Are you having dejà vu? This just in! From the "Read the entire problem before you start" dept: This problem was on a previous assignment. You may skip it if you've already done it, just include a comment saying that you've already done it. If you've now done the problem twice, perhaps next time you'll read the whole problem before starting it.
+//done it
+
+// What are the things in an array called?
+  //elements
+// Do Arrays guarantee those things will be in order?
+  //yes, it is a data strucutre. the order that the indexes start with 0 being the first element.
+// What real-life thing could you model with an array?
+  // a grocery list, contains all the elements you need, but will need to know where to find it to access the element.
