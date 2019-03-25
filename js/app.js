a58405a70bc9b85e2f15d35c84f7d66b7429607b
Destiny <dshillis1@gmail.com>
2019-03-24 19:59:27 -0600

    V. Objects, B. Update the user done

--- js/app.js
+++ js/app.js
@@ -792,5 +792,20 @@ console.log(user);
 
 
 
+// C. Adding keys and values
+// You have decided to add your user's location to the data that you want to 
+//collect.
+
+//1. Without changing the original user object, add a new key location to the 
+//object, and give it a value or some-or-other location (a string).
+
+user.location = 'Denver, CO';
+console.log(user);
+
+
+
+
+
+
 
 
