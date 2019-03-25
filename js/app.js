8e71db46959f48275fa79bb512b95d8f75f10b45
shadiboi <saoutabachi@gmail.com>
2019-03-24 21:10:11 -0600

    III. Arrays & Control flow: Whats in your closet?

--- js/app.js
+++ js/app.js
@@ -362,64 +362,69 @@ if (integer < 5) {
     console.log("BIG NUMBER")
 } else (console.log("MONKEY"));
 
-// :red_circle: Commit.
+// :red_circle: Commit. //DONE
 
 
 // ### H. What's in Your Closet?
 
 // Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript.
 
-// ```javascript
-// const kristynsCloset = [
-//   "left shoe",
-//   "cowboy boots",
-//   "right sock",
-//   "GA hoodie",
-//   "green pants",
-//   "yellow knit hat",
-//   "marshmallow peeps"
-// ];
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
 
-// // Thom's closet is more complicated. Check out this nested data structure!!
-// const thomsCloset = [
-//   [
-//     // These are Thom's shirts
-//     "grey button-up",
-//     "dark grey button-up",
-//     "light blue button-up",
-//     "blue button-up",
-//   ],[
-//     // These are Thom's pants
-//     "grey jeans",
-//     "jeans",
-//     "PJs"
-//   ],[
-//     // Thom's accessories
-//     "wool mittens",
-//     "wool scarf",
-//     "raybans"
-//   ]
-// ];
-// ```
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
 
 // 1. What's Kristyn wearing today? Using bracket notation to access items in `kristynsCloset`, log the sentence "Kristyn is rocking that " + *the third item in Kristyn's closet* + " today!" to the console.
+console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!");
 
 // 2. Kristyn just bought some sweet shades! Add `"raybans"` to her closet **after `"yellow knit hat"`.**
+kristynsCloset.splice(6,1,"raybans");  //HOW DO I KEEP ITEMS FOLLOWING RAYBANS
 
 // 3. Kristyn spilled coffee on her hat... modify this item to read `"stained knit hat"` instead of yellow.
-
+kristynsCloset[5] = "Stained knit hat";
 // 4. Put together an outfit for Thom! Using **bracket notation**, access the first element in Thom's `shirts` array.
+const thomOutfit = [thomsCloset[0][0]]; 
 
 // 5. In the same way, access one item from Thom's pants array.
+thomOutfit.push(thomsCloset[1][0]); 
 
 // 6. Access one item from Thom's accessories array.
+thomOutfit.push(thomsCloset[2][0]); 
 
 // 7. Log a sentence about what Thom's wearing. Example: `"Thom is looking fierce in a grey button-up, jeans and wool scarf!"`
+console.log(`Thom is wearing a ${thomOutfit[0]}, with ${thomOutfit[1]}, and some sick ass ${thomOutfit[2]}!`);
 
 // 8. Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to `Footie Pajamas`.
-
+thomsCloset[1][2] = "Footie PJs";
 // :red_circle: Commit.
-
+console.log(kristynsCloset);
+console.log(thomOutfit);
 
 
 
